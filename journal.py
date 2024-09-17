08b0cef73dbec87099ecac8b72de16ba38b141b6
H.N. Gibson <revhngibson@gmail.com>
2024-09-17 15:20:08 -0500

    added to Journal

---
+++ journal.py
@@ -0,0 +1,7 @@
+#Create a function for adding an entry to Journal.txt
+
+#Create a function for adding a new Weekly summary
+
+#Write a program to manage adding new Journal entries and Weekly Summaries
+    #Have Journal Entries automate the date and look for 1 line of text
+    #Have Weekly Entries loop to ask for more lines if necessary
